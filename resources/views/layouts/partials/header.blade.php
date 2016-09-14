3420933eb396b3e5ad21729719019b9a0122a419
Joe Sprankle <joe@2e18.com>
2016-09-14 13:14:46 -0700

    fix to header needed

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -102,22 +102,12 @@
                         <li class="user-header">
                             <img src="{{$currentSchool->school_logo}}" class="img-circle" alt="User Image">
                             <p>
-                                {{$currentSchool->name}} - {{$currentSchool->city}}
-                                <small>Member since Nov. 2012</small>
+                                {{$currentSchool->name}} - {{$currentSchool->state}}
+                                <small>{{$currentSchool->city}}</small>
                             </p>
                         </li>
                         <!-- Menu Body -->
-                        <li class="user-body">
-                            <div class="col-xs-4 text-center">
-                                <a href="#">Followers</a>
-                            </div>
-                            <div class="col-xs-4 text-center">
-                                <a href="#">Sales</a>
-                            </div>
-                            <div class="col-xs-4 text-center">
-                                <a href="#">Friends</a>
-                            </div>
-                        </li>
+
                         <!-- Menu Footer-->
                         <li class="user-footer">
                             <div class="pull-left">
